04dafdd1865a6254e0c60bd40bcafe3d941e10c8
Gourab <developer.gourab@gmail.com>
2019-12-02 15:31:49 +0530

    First Commint

---
+++ ezypayaepssdk/src/main/java/com/example/ezypayaepssdk/EzypayAEPS.kt
@@ -0,0 +1,13 @@
+package com.example.ezypayaepssdk
+
+import android.util.Log
+
+class EzypayAEPS {
+
+    fun d(message: String) {
+
+        Log.d("TEST APES","SUPER GOURAB")
+
+        
+    }
+}
