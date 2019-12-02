3eb627b96d12c31983b33b7c75965b63148bb18d
Gourab <developer.gourab@gmail.com>
2019-12-02 18:27:00 +0530

    AEPS First Update 02.12.2019

---
+++ app/src/main/java/com/samplesdk/Constants.java
@@ -0,0 +1,26 @@
+package com.samplesdk;
+
+/**
+ * Created by Developer on 6/12/2017.
+ */
+
+public class Constants {
+
+
+    public static String SERVICE_MICRO_TS = "154";
+    public static String SERVICE_MICRO_CW = "156";
+    public static String SERVICE_AEPS_CW = "151";
+    public static String SERVICE_AEPS_BE = "152";
+    public static String SERVICE_MICRO_BE = "157";
+    public static String SERVICE_AEPS_TS   = "158";
+
+    public static String MERCHANT_ID ="9434710602";
+    public static String CLIENT_HEADER_ENCRYPTION_KEY ="982b0d01-b262-4ece-a2a2-45be82212ba1";
+    public static String CLIENT_REQUEST_ENCRYPTION_KEY = "dbdc2c5a-847b-4b85-9a06-b0bbc901cdd9"; // 10 meters
+    public static String RETURN_URL= "https://180.179.20.116:8061/response.aspx";
+    public static String VERSION= "100";
+    public static String AUTHKEY= "d134287d-662a-4d5f-bca7-7224ae978135";
+    public static String CLIENTID= "13";
+    public static String RADIOUS= "5000";
+
+}
