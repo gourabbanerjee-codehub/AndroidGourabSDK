116c0fb67851c181d86cb08b876c36cf56bf2d0e
Gourab <developer.gourab@gmail.com>
2019-12-03 13:22:34 +0530

    AEPS 2nd Update 03.12.2019

--- app/src/main/java/com/samplesdk/MainActivity.kt
+++ app/src/main/java/com/samplesdk/MainActivity.kt
@@ -12,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        val intent= Intent(this,EzypayAEPS::class.java)
+        startActivity(intent)
+
+
     }
 }
--- ezypayaepssdk/src/main/java/com/example/ezypayaepssdk/EzypayAEPS.kt
+++ ezypayaepssdk/src/main/java/com/example/ezypayaepssdk/EzypayAEPS.kt
@@ -1,13 +1,17 @@
 package com.example.ezypayaepssdk
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 
-class EzypayAEPS {
+class EzypayAEPS : AppCompatActivity(){
 
-    fun d(message: String) {
 
-        Log.d("TEST APES","SUPER GOURAB")
-
-        
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test_aeps)
     }
 }
